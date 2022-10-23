8e39e08bd033153d0df4da7afd980bab011326bc
Konrad <k.konrad9902@gmail.com>
2022-10-24 01:12:58 +0200

    feat: added registartion form page with axios

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import Header from "./components/Header"
+import Header from "./components/Header";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Register from "./pages/Register";
 function App() {
   return (
     <div className="App">
-      <Header/>
+      <Header />
+      <Register />
     </div>
   );
 }
