1e8eddea3bcc0d125ab7c5b8915520e9c9984e2c
Kacper-kas <kacper.kas@outlook.com>
2022-10-30 22:20:02 +0100

    feat: Navigation buttons and WiadomosciView ready

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import Footer from "./components/Footer";
 import Login from "./components/Login";
 import PlanZajec from "./components/PlanZajec";
 import Frekwencja from "./components/Frekwencja";
+import Wiadomosci from "./components/Wiadomosci";
+import Oceny from "./components/Oceny";
+import Terminarz from "./components/Terminarz";
 import { Component } from "react";
 import {Row, Col} from 'react-bootstrap';
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -16,7 +19,10 @@ class App extends Component {
       showHideSummary: true,
       showHideLogin: false,
       showHidePlanZajec: false,
-      showHideFrekwencja: false
+      showHideFrekwencja: false,
+      showHideWiadomosci: false,
+      showHideOceny: false,
+      showHideTerminarz: false
     };
     this.hideComponent = this.hideComponent.bind(this);
   }
@@ -28,22 +34,55 @@ class App extends Component {
       //   this.setState({ showHideSummary: !this.state.showHideSummary });
       //   break;
       case "showHideLogin":
-        this.setState({ showHideSummary: !this.state.showHideSummary });
+        this.setState({ showHideSummary: false });
         this.setState({ showHideFrekwencja: false });
         this.setState({ showHidePlanZajec: false });
-        this.setState({ showHideLogin: !this.state.showHideLogin });
+        this.setState({ showHideLogin: true });
+        this.setState({ showHideTerminarz: false });
+        this.setState({ showHideOceny: false });
         break;
       case "showHidePlanZajec":
         this.setState({ showHideSummary: false });
         this.setState({ showHideFrekwencja: false });
         this.setState({ showHidePlanZajec: true });
         this.setState({ showHideLogin: false });
+        this.setState({ showHideTerminarz: false });
+        this.setState({ showHideOceny: false });
         break;
       case "showHideFrekwencja":
         this.setState({ showHideSummary: false });
         this.setState({ showHidePlanZajec: false });
         this.setState({ showHideLogin: false });
         this.setState({ showHideFrekwencja: true });
+        this.setState({ showHideTerminarz: false });
+        this.setState({ showHideOceny: false });
+        break;
+      case "showHideWiadomosci":
+        this.setState({ showHideSummary: false });
+        this.setState({ showHidePlanZajec: false });
+        this.setState({ showHideLogin: false });
+        this.setState({ showHideFrekwencja: false });
+        this.setState({ showHideWiadomosci: true });
+        this.setState({ showHideTerminarz: false });
+        this.setState({ showHideOceny: false });
+        break;
+      case "showHideOceny":
+        this.setState({ showHideSummary: false });
+        this.setState({ showHidePlanZajec: false });
+        this.setState({ showHideLogin: false });
+        this.setState({ showHideFrekwencja: false });
+        this.setState({ showHideWiadomosci: false });
+        this.setState({ showHideTerminarz: false });
+        this.setState({ showHideOceny: true });
+        break;
+      case "showHideTerminarz":
+        this.setState({ showHideSummary: false });
+        this.setState({ showHidePlanZajec: false });
+        this.setState({ showHideLogin: false });
+        this.setState({ showHideFrekwencja: false });
+        this.setState({ showHideWiadomosci: false });
+        this.setState({ showHideTerminarz: true });
+        this.setState({ showHideOceny: false });
         break;
       default:
         this.setState({ showHideSummary: true });
@@ -51,7 +90,7 @@ class App extends Component {
   }
 
   render() {
-    const { showHideSummary, showHideLogin, showHidePlanZajec, showHideFrekwencja } = this.state;
+    const { showHideSummary, showHideLogin, showHidePlanZajec, showHideFrekwencja, showHideWiadomosci,showHideOceny,showHideTerminarz } = this.state;
     return (
       <div class="row gy-4"> 
         <Header/>
@@ -59,9 +98,9 @@ class App extends Component {
         <div id='przyciski' class="row gy-3 "> 
         <Col className='col-md-1'></Col>
           <Col><button type="button" onClick={() => this.hideComponent("showHidePlanZajec")}  class="btn btn-primary">Plan zajęć</button></Col>
-          <Col><button type="button" onClick={() => this.hideComponent("showHideLogin")}  class="btn btn-danger">Oceny</button></Col>
-          <Col><button type="button" onClick={() => this.hideComponent("showHidePlanZajec")} class="btn btn-success">Terminarz</button></Col>
-          <Col><button type="button" onClick={() => this.hideComponent("showHidePlanZajec")} class="btn btn-warning">Wiadomości</button></Col>
+          <Col><button type="button" onClick={() => this.hideComponent("showHideOceny")}  class="btn btn-danger">Oceny</button></Col>
+          <Col><button type="button" onClick={() => this.hideComponent("showHideTerminarz")} class="btn btn-success">Terminarz</button></Col>
+          <Col><button type="button" onClick={() => this.hideComponent("showHideWiadomosci")} class="btn btn-warning">Wiadomości</button></Col>
           <Col><button type="button" onClick={() => this.hideComponent("showHideFrekwencja")} class="btn btn-info">Frekwencja</button></Col>
           </div>
         </Row>
@@ -71,6 +110,9 @@ class App extends Component {
           {showHideLogin && <Login />}
           {showHidePlanZajec && <PlanZajec />}
           {showHideFrekwencja && <Frekwencja />}
+          {showHideWiadomosci && <Wiadomosci />}
+          {showHideOceny && <Oceny />}
+          {showHideTerminarz && <Terminarz />}
           </div>
           </Row>
         <Footer/>
---
+++ src/components/Oceny.js
@@ -0,0 +1,26 @@
+import {useState} from 'react';
+import "./style/custom.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import {Container, Row, Col } from 'react-bootstrap';
+// import ButtonRow from "./ButtonRow";
+
+
+function Oceny() {
+ 
+  return (
+    <Container className="justify-content-md-center min-vh-100">
+      {/* <ButtonRow/> */}
+      
+      <Row className="text-center">
+      {/* Ten div niżej to jest odstep od gornej belki */}
+        <div class="row gy-3"> 
+        <h3>Oceny</h3><br/>
+
+    </div>
+        </Row>
+      
+  </Container>
+  );
+}
+
+export default Oceny;
